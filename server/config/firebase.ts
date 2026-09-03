2e3de306e7bc5a2cb83bc6a1b5facc2ad4413639
anvantu2-create <anvantu2@gmail.com>
2026-09-03 14:35:54 +0700

    mvp photos: update server/config/firebase.ts

--- server/config/firebase.ts
+++ server/config/firebase.ts
@@ -1,6 +1,23 @@
-import{applicationDefault,cert,getApps,initializeApp}from'firebase-admin/app';
-import{getAuth}from'firebase-admin/auth';import{getFirestore}from'firebase-admin/firestore';
-const projectId=process.env.FIREBASE_PROJECT_ID;
-const credential=process.env.FIREBASE_SERVICE_ACCOUNT_JSON?cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON)):applicationDefault();
-const app=getApps()[0]??initializeApp({credential,projectId});
-export const auth=getAuth(app);export const db=getFirestore(app);
+import {
+  applicationDefault,
+  cert,
+  getApps,
+  initializeApp,
+} from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const credential = process.env.FIREBASE_SERVICE_ACCOUNT_JSON
+  ? cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON))
+  : applicationDefault();
+const app =
+  getApps()[0] ??
+  initializeApp({
+    credential,
+    projectId,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  });
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const bucket = getStorage(app).bucket();
