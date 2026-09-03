7864d7911a97eb479386f10f866fd244635caa4b
anvantu2-create <anvantu2@gmail.com>
2026-09-03 11:38:32 +0700

    chore: add src/types.ts

---
+++ src/types.ts
@@ -0,0 +1,2 @@
+export type Page='dashboard'|'substations'|'feeders'|'devices'|'loops';
+export interface Device{id:string;code:string;name:string;type:'REC'|'LBS'|'DS'|'RMU';station:string;feeder:string;status:'Đóng'|'Mở';enabled:boolean;pole:string;setting:string}
