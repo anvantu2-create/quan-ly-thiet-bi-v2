b76e299146530fceac9748315cae59f0b2f0a1a8
anvantu2-create <anvantu2@gmail.com>
2026-09-03 14:16:28 +0700

    mvp users: update src/types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,14 @@
-export type Page='dashboard'|'substations'|'feeders'|'devices'|'loops';
-export interface Device{id:string;code:string;name:string;type:'REC'|'LBS'|'DS'|'RMU';station:string;feeder:string;status:'Đóng'|'Mở';enabled:boolean;pole:string;setting:string}
+export type Page =
+  "dashboard" | "substations" | "feeders" | "devices" | "loops" | "users";
+export interface Device {
+  id: string;
+  code: string;
+  name: string;
+  type: "REC" | "LBS" | "DS" | "RMU";
+  station: string;
+  feeder: string;
+  status: "Đóng" | "Mở";
+  enabled: boolean;
+  pole: string;
+  setting: string;
+}
